4f70ea7d801ee94d6e2a3c13eb81d1988bc9e7d6
lihewei <1968895380@qq.com>
2019-12-26 11:09:46 +0800

    第一次提交

--- src/main/java/com/taoding/service/AdvertisementThrowinService.java
+++
@@ -1,9 +0,0 @@
-package com.taoding.service;
-
-/**
- * @ClassName: AdvertisementThrowinService
- * @Author: lihewei
- * @Date: 2019-12-26 10:13
- */
-public interface AdvertisementThrowinService {
-}
